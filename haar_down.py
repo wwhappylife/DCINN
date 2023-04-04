44bf2845400eb88cbbe062758595d4aa95500a86
wwhappylife <wwhappylife@163.com>
2023-04-04 10:59:32 +0800

    Add files via upload

---
+++ haar_down.py
@@ -0,0 +1,46 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+
+class HaarDownsampling(nn.Module):
+    def __init__(self, channel_in):
+        super(HaarDownsampling, self).__init__()
+        self.channel_in = channel_in
+
+        self.haar_weights = torch.ones(4, 1, 2, 2)
+
+        self.haar_weights[1, 0, 0, 1] = -1
+        self.haar_weights[1, 0, 1, 1] = -1
+
+        self.haar_weights[2, 0, 1, 0] = -1
+        self.haar_weights[2, 0, 1, 1] = -1
+
+        self.haar_weights[3, 0, 1, 0] = -1
+        self.haar_weights[3, 0, 0, 1] = -1
+
+        self.haar_weights = torch.cat([self.haar_weights] * self.channel_in, 0)
+        self.haar_weights = nn.Parameter(self.haar_weights)
+        self.haar_weights.requires_grad = False
+
+    def forward(self, x, rev=False):
+        if not rev:
+            self.elements = x.shape[1] * x.shape[2] * x.shape[3]
+            self.last_jac = self.elements / 4 * np.log(1/16.)
+
+            out = F.conv2d(x, self.haar_weights, bias=None, stride=2, groups=self.channel_in) / 4.0
+            out = out.reshape([x.shape[0], self.channel_in, 4, x.shape[2] // 2, x.shape[3] // 2])
+            out = torch.transpose(out, 1, 2)
+            out = out.reshape([x.shape[0], self.channel_in * 4, x.shape[2] // 2, x.shape[3] // 2])
+            return out
+        else:
+            self.elements = x.shape[1] * x.shape[2] * x.shape[3]
+            self.last_jac = self.elements / 4 * np.log(16.)
+
+            out = x.reshape([x.shape[0], 4, self.channel_in, x.shape[2], x.shape[3]])
+            out = torch.transpose(out, 1, 2)
+            out = out.reshape([x.shape[0], self.channel_in * 4, x.shape[2], x.shape[3]])
+            return F.conv_transpose2d(out, self.haar_weights, bias=None, stride=2, groups = self.channel_in)
+
+    def jacobian(self, x, rev=False):
+        return self.last_jac
